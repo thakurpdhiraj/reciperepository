5813ee285dbcee4715715c8f64848b45cb2bf2ab
C67845 <C67845@nl0311AB36746.adm.ab3.abn.ssm.sdc.gts.ibm.com>
2019-09-09 18:51:14 +0530

    Removed getRecipe() added testForRecipeName(),testForRecipeSize()

--- recipe/src/test/java/com/tcs/demo/recipe/test/RecipeApiTest.java
+++ recipe/src/test/java/com/tcs/demo/recipe/test/RecipeApiTest.java
@@ -3,61 +3,84 @@
  */
 package com.tcs.demo.recipe.test;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertEquals;
 
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.Base64;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.tcs.demo.recipe.RecipeApplication;
 import com.tcs.demo.recipe.bean.Recipe;
 import com.tcs.demo.recipe.bean.Recipe.PeopleGroup;
+import com.tcs.demo.recipe.repository.RecipeRespository;
 import com.tcs.demo.recipe.service.RecipeService;
+import com.tcs.demo.recipe.service.RecipeServiceImpl;
 /**
  * @author Dhiraj
  *
  */
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = RecipeApplication.class)
-@SpringBootTest()
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RecipeApiTest {
 
+
+	@LocalServerPort
+	private int port;
+
+	@Mock
+	RecipeRespository recipeRepository;
+
+	@InjectMocks
+	RecipeService recipeServie = new RecipeServiceImpl();
+
 	@Autowired
-	private WebApplicationContext wac;
+	private TestRestTemplate restTemplate;
 
-	private MockMvc mockMvc;
-
-	@MockBean
-	RecipeService recipeServie;
-
-	@Before
-	public void setup() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-
-	}
 
 	@Test
-	public void getRecipe() throws Exception{
-		Recipe recipe = new Recipe(1L, "Caponata Pasta", "olive oil,onion,tomatoes,small capers,raisins,rigatoni", "Heat the oil in a large pan and cook the onion for 8-10 mins until starting to caramelise (or for longer if you have time – the sweeter the better). Add the garlic for the final 2 mins of cooking time.Tip in the mixed veg, tomatoes, capers and raisins. Season well and simmer, uncovered, for 10 mins, or until you have a rich sauce.Meanwhile, boil the kettle. Pour the kettleful of water into a large pan with a little salt and bring back to the boil. Add the pasta and cook until tender with a little bite, then drain, reserving some of the pasta water. Tip the pasta into the sauce, adding a splash of pasta water if it needs loosening. Scatter with the basil leaves and parmesan, if you like, and serve straight from the pan.", "image/caponata-pasta.jpg", true,PeopleGroup.FIVETOTEN,
-				1L, LocalDateTime.now(), null, null,1);
+	public void testForRecipeName() throws Exception{
+		Recipe recipe = new Recipe();
+		recipe.setRcpName("Caponata Pasta");
+	
+		String base64Creds = Base64.getEncoder() 
+				.encodeToString(("admin:password") .getBytes());
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic " + base64Creds);
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+		ResponseEntity<Recipe> response = restTemplate
+				.exchange(new URL("http://localhost:" + port + "/kitchenworld/api/recipes/1").toString(), HttpMethod.GET, request, Recipe.class);
 
-		given(recipeServie.getRecipe(1L)).willReturn(recipe);
+		assertEquals(recipe.getRcpName(), response.getBody().getRcpName());
+	}
+	
+	@Test
+	public void testForRecipeSize() throws Exception{
+		String base64Creds = Base64.getEncoder() 
+				.encodeToString(("admin:password") .getBytes());
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic " + base64Creds);
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+		ResponseEntity<String> response = restTemplate
+				.exchange(new URL("http://localhost:" + port + "/kitchenworld/api/recipes/size").toString(), HttpMethod.GET, request, String.class);
 
-		mockMvc.perform(get("kitchenworld/api/recipes/1").with(user("admin").password("password")))
-		.andExpect(status().isOk()).andExpect(content().json("{\"rcpId\":1,\"rcpName\":\"Caponata Pasta\",\"rcpIngredientDescription\":\"olive oil,onion,tomatoes,small capers,raisins,rigatoni\",\"rcpCookingInstruction\":\"Heat the oil in a large pan and cook the onion for 8-10 mins until starting to caramelise (or for longer if you have time – the sweeter the better). Add the garlic for the final 2 mins of cooking time.Tip in the mixed veg, tomatoes, capers and raisins. Season well and simmer, uncovered, for 10 mins, or until you have a rich sauce.Meanwhile, boil the kettle. Pour the kettleful of water into a large pan with a little salt and bring back to the boil. Add the pasta and cook until tender with a little bite, then drain, reserving some of the pasta water. Tip the pasta into the sauce, adding a splash of pasta water if it needs loosening. Scatter with the basil leaves and parmesan, if you like, and serve straight from the pan.\",\"rcpImagePath\":\"image/caponata-pasta.jpg\",\"rcpIsVegetarian\":true,\"rcpSuitableFor\":\"FIVETOTEN\",\"rcpCreatedBy\":1,\"rcpCreatedAt\":\"09-09-2019 00:19\",\"rcpUpdatedBy\":null,\"rcpUpdatedAt\":\"09-09-2019 00:19\",\"rcpRowState\":1}"));
+
+		assertEquals("7", response.getBody());
 	}
 }
