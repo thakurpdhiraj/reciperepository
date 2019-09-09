6a6ae64640234b0e31348a3de6a8473fbdd261d6
Dhiraj <Dhiraj@DESKTOP-54JMJOR>
2019-09-10 00:53:48 +0530

    Adding ResponseStatus  annotation to RecipeNotFoundException

--- recipe/src/main/java/com/tcs/demo/recipe/controller/RecipeController.java
+++ recipe/src/main/java/com/tcs/demo/recipe/controller/RecipeController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -297,6 +298,7 @@ public class RecipeController {
 	 * @author Dhiraj
 	 *
 	 */
+	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public class RecipeNotFoundException extends RuntimeException{
 
 		private static final long serialVersionUID = 1L;
