a4666b36a3c96e659f916f5b235cb23914445224
Dhiraj <Dhiraj@DESKTOP-54JMJOR>
2019-09-11 06:35:36 +0530

    Adding ApiInfo

--- recipe/src/main/java/com/tcs/demo/recipe/config/SwaggerConfig.java
+++ recipe/src/main/java/com/tcs/demo/recipe/config/SwaggerConfig.java
@@ -1,10 +1,15 @@
 package com.tcs.demo.recipe.config;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,11 +18,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	@Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()                 
-                .apis(RequestHandlerSelectors.basePackage("com.tcs.demo.recipe.controller"))
-                .paths(PathSelectors.ant("/api/**"))
-                .build();
-    }
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()                 
+				.apis(RequestHandlerSelectors.basePackage("com.tcs.demo.recipe.controller"))
+				.paths(PathSelectors.ant("/api/**"))
+				.build()
+				.apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		@SuppressWarnings("rawtypes")
+		ApiInfo apiInfo = new ApiInfo(
+				"Recipe Application",
+				"API which allows users to manage their favourite recipe with basic CRUD operations",
+				"1.0",
+				"Terms of service",
+				new Contact("Dhiraj", "https://github.com/dhi37th/reciperepository.git", ""),
+				"",
+				"", new ArrayList<VendorExtension>());
+		return apiInfo;
+	}
 }
