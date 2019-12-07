50b0089cde16edf02b97e35c5bfc0c6f697402a4
Dhiraj Thakur <dhi37th@gmail.com>
2019-12-07 18:48:29 +0530

    First commit

---
+++ muquestionpaper/src/main/java/com/dhita/WicketApplication.java
@@ -0,0 +1,23 @@
+package com.dhita;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+
+public class WicketApplication extends WebApplication
+{
+
+	@Override
+	public Class<? extends WebPage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}
+}
